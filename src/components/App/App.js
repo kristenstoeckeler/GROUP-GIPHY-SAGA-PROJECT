a8cb69ea65d75687ef94e6b9be010038deb45f61
zbattaglia <zbattaglia3@gmail.com>
2020-04-16 11:50:59 -0500

    linked to search and favorites pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
+//links
+import GiphySearch from '../GiphySearch/GiphySearch';
+import FavoritesPage from '../FavoritesPage/FavoritesPage';
 
 class App extends Component {
 
+
   render() {
     return (
-      <div>
+      <div className="App">
+        <Router>
+          <Route exact path='/' component={ GiphySearch } />
+          <Route exact path='/favorites' component={ FavoritesPage } />
+        </Router>
       </div>
     );
   }
 
 }
 
-export default App;
+export default withRouter(App);
