a68d0927ffe16ac84a1eaa7d956056eaca0829da
zbattaglia <zbattaglia3@gmail.com>
2020-04-16 13:18:26 -0500

    Added a navbar to switch between pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,24 @@ import { withRouter } from 'react-router';
 //links
 import GiphySearch from '../GiphySearch/GiphySearch';
 import FavoritesPage from '../FavoritesPage/FavoritesPage';
+import './App.css';
 
 class App extends Component {
+
+  handleClick = (event, linkTo) => {
+    console.log( 'Got click on', linkTo );
+    this.props.history.push( linkTo );
+  }
 
 
   render() {
     return (
       <div className="App">
         <Router>
+          <nav className='navBar'>
+            <p className = "link"><Link to='/favorites'>See Favorites </Link></p>
+            <p className = "link"><Link to='/'>Search for more Gifs </Link></p>
+          </nav>
           <Route exact path='/' component={ GiphySearch } />
           <Route exact path='/favorites' component={ FavoritesPage } />
         </Router>
